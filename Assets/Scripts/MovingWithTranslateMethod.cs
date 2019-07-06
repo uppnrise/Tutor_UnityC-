33914bf63a3f7ce1bcc7ff7ba7de28f8d7e8966e
Burhanettin NACAR <mtbnacar@gmail.com>
2019-07-06 19:29:56 +0300

    translate method usage and explanation

---
+++ Assets/Scripts/MovingWithTranslateMethod.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingWithTranslateMethod : MonoBehaviour
+{
+    public float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // A good way of moving an object is to use translate method.
+        // if you don't use Time.deltaTime object will move too fast.
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+    }
+}
