ce57ecbe2b8728cd8c22ad181915f7eca9588039
Burhanettin NACAR <mtbnacar@gmail.com>
2019-07-06 19:09:15 +0300

    transform position implementation

---
+++ Assets/Scripts/TransformComponent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformComponent : MonoBehaviour
+{
+    Vector3 tempPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tempPos = transform.position;
+
+        tempPos.x += 5f;
+        // cannot change this value directly.
+        // That is why we have to use a temporary variable.
+        transform.position = tempPos;
+
+
+        // Unity uses quaternions for rotation
+        // increase x, y or z to change the rotation
+        transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z);
+        Vector3 scale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
