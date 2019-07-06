d60aee4e6e5c25fe98dcb86a18b7db99b6557b41
Burhanettin NACAR <mtbnacar@gmail.com>
2019-07-06 12:26:17 +0300

    Unity c sharp first program

---
+++ Assets/Scripts/FirstCSharpProgram.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ If you want to render something to screen your class needs
+ to inherit from Unity's MonoBehaviour base class.
+ */
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update");
+    }
+}
