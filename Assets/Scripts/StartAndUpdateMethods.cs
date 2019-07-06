8e66d4bd071f0c7739e0fff7e8876a51386bd222
Burhanettin NACAR <mtbnacar@gmail.com>
2019-07-06 12:37:16 +0300

    Start and Update methods are beriefly explained

---
+++ Assets/Scripts/StartAndUpdateMethods.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAndUpdateMethod : MonoBehaviour
+{
+    /* Start is called before the first frame update
+     * and only one time.
+     * Only called if script is enabled.
+     */
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update");
+    }
+}
