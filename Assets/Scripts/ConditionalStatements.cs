568e02eb16a98a0c0fef3e1ad480a9365856d2bc
Burhanettin NACAR <mtbnacar@gmail.com>
2019-07-06 13:12:48 +0300

    if and else conditional statements are briefly explaned over a health point example

---
+++ Assets/Scripts/ConditionalStatements.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConditionalStatements : MonoBehaviour
+{
+    int healthPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthPoint = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (healthPoint < 1)
+        {
+            Debug.Log("Game Over");
+        }
+        else
+        {
+            Debug.Log("Still fighting");
+        }
+    }
+}
