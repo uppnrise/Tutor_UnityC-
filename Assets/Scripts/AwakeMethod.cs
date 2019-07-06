8709d93139b00b23f5ef9a031ca83d53ecfc1be6
Burhanettin NACAR <mtbnacar@gmail.com>
2019-07-06 12:43:22 +0300

    Awake method is explained briefly

---
+++ Assets/Scripts/AwakeMethod.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeMethod : MonoBehaviour
+{
+    // Gets called before Start()
+    // Called everytime even if script is not enabled.
+    void Awake()
+    {
+        Debug.Log("Awake");
+    }
+
+    // Start is called before the first frame update
+    // Only called if script is enabled.
+    // Gets called after Awake()
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
