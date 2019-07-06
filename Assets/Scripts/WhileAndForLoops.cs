4cf0545cbf8274f0e6d3708317118eb598d7356b
Burhanettin NACAR <mtbnacar@gmail.com>
2019-07-06 13:38:48 +0300

    while and for loop examples and general loop concepts are explained

---
+++ Assets/Scripts/WhileAndForLoops.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhileAndForLoops : MonoBehaviour
+{
+
+    int counter = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (counter != 0)
+        {
+            Debug.Log("value of counter = " + counter);
+            counter--; // counter = counter - 1;
+        }
+
+        Debug.Log("outside of while loop, before for loop");
+
+        for(int i = 0 ; i < 5 ; i++)
+        {
+            Debug.Log("value of i: " + i);
+        }
+
+        Debug.Log("outside of for and while loop, after for loop");
+    }
+}
