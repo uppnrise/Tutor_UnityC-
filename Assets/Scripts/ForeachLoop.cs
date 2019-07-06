2ed3edc8e4b335da121699b7bda43392c45adb3a
Burhanettin NACAR <mtbnacar@gmail.com>
2019-07-06 18:38:31 +0300

    foreach looping

--- Assets/Scripts/ForeachLoop.cs
+++ Assets/Scripts/ForeachLoop.cs
@@ -2,19 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Arrays : MonoBehaviour
+public class ForeachLoop : MonoBehaviour
 {
-    int size;
-
-    // if it is public everyone can access it
-    // even from GUI of Unity.
     public int[] array;
 
     // Start is called before the first frame update
     void Start()
     {
-        size = 10;
-        array = new int[size];
+        // 
+        foreach(int i in array)
+        {
+            Debug.Log(i);
+        }
+        
     }
 
     // Update is called once per frame
