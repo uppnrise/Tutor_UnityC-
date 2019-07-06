d20e7c74514704a7fe2e6be300b91b4612e69810
Burhanettin NACAR <mtbnacar@gmail.com>
2019-07-06 13:06:37 +0300

    Destroying a cube game object

---
+++ Assets/Scripts/DestroyingObjects.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyingObjects : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Destroy a specific game object after 3 second!
+        // Attach this script to a game object and it will destroy it.
+        Destroy(gameObject, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
