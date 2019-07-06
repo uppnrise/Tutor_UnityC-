da17313d033da63550ecc5bc8718c81ec460f484
Burhanettin NACAR <mtbnacar@gmail.com>
2019-07-06 18:16:56 +0300

    Simple implementation of arrays

---
+++ Assets/Scripts/Arrays.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour
+{
+    int size;
+
+    // if it is public everyone can access it
+    // even from GUI of Unity.
+    public int[] array;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        size = 10;
+        array = new int[size];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
