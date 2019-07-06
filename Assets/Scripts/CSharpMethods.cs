2e9d043958c832f9fa5cfb43ff380b5916f5639a
Burhanettin NACAR <mtbnacar@gmail.com>
2019-07-06 12:55:36 +0300

    c sharp simple method call and implementation

---
+++ Assets/Scripts/CSharpMethods.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSharpMethods : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int value = Evaluate(2);
+
+        Debug.Log(value);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    int Evaluate(int data)
+    {
+        Debug.Log("Evaluate");
+        Debug.Log(data);
+
+        return (data * 5);
+    }
+}
