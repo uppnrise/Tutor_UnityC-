ee0790fe7d07e32f79edffed36a5f55dcbef824f
Burhanettin NACAR <mtbnacar@gmail.com>
2019-07-07 17:33:01 +0300

    transform.Rotate method is explained briefly

---
+++ Assets/Scripts/RotationGameObjects.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationGameObjects : MonoBehaviour
+{
+    public float rot = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(5f, rot, 0);
+    }
+}
