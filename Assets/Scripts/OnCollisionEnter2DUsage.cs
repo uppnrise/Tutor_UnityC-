55129b40002521d8a17bf4794829729e7f41216a
Burhanettin NACAR <mtbnacar@gmail.com>
2019-07-07 20:47:41 +0300

    onCollisionEnter2D method is explained briefly

---
+++ Assets/Scripts/OnCollisionEnter2DUsage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollisionEnter2DUsage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Getting called if attached object gets collided with something.
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Colliding");
+        // if collided object has a tag name "nacar", destroy it.
+        if(collision.gameObject.tag == "nacar")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
