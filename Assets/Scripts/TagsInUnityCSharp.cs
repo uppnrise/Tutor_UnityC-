298ee7775aadecf443d65160fc62c3fa929ec210
Burhanettin NACAR <mtbnacar@gmail.com>
2019-07-07 19:33:58 +0300

    Tag concept is explained briefly

---
+++ Assets/Scripts/TagsInUnityCSharp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagsInUnityCSharp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(this.gameObject.tag == "Quad")
+        {
+            Debug.Log("This is a quad");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
