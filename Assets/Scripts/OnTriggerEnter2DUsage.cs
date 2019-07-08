64739479d487e216369acd26688474167f6a639a
Burhanettin NACAR <mtbnacar@gmail.com>
2019-07-08 21:43:51 +0300

    OnTriggerEnter2D, SerializeField and HideInInspector are explained briefly

---
+++ Assets/Scripts/OnTriggerEnter2DUsage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerEnter2DUsage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Use it when you have a trigger game object which
+    // allows you to pass through inside.
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "trig")
+            Destroy(collision.gameObject);
+    }
+}
---
+++ Assets/Scripts/SerializeFieldAndHideInInspector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SerializeFieldAndHideInInspector : MonoBehaviour
+{
+    // we can't access it from another class/script
+    // but we can assign a value from GUI/Scene Screen.
+    [SerializeField]
+    private int value;
+
+    // if we want to hide in inspector a public field,
+    // use below tag.
+    [HideInInspector]
+    public int value1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
