de3fa85e214e6f9964f755ae3f9f2bc9fbc715eb
Burhanettin NACAR <mtbnacar@gmail.com>
2019-07-08 23:06:40 +0300

    InvokeRepeating method usage in a simple way

---
+++ Assets/Scripts/InvokeRepeatingMethod.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvokeRepeatingMethod : MonoBehaviour
+{
+    public GameObject ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // call inst, 1 time in 1 second.
+        InvokeRepeating("inst", 1f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void inst()
+    {
+        Instantiate(ball, transform.position, transform.rotation);
+    }
+}
