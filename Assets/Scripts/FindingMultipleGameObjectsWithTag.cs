d00426124c72e7b918bba78e923cfe74b71270e2
Burhanettin NACAR <mtbnacar@gmail.com>
2019-07-08 22:09:35 +0300

    finding multiple game objects with FindGameObjectsWithTag

---
+++ Assets/Scripts/FindingMultipleGameObjectsWithTag.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindingMultipleGameObjectsWithTag : MonoBehaviour
+{
+    GameObject[] QuadArray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        QuadArray = GameObject.FindGameObjectsWithTag("Quad");
+
+        foreach (GameObject gameObject in QuadArray)
+            Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
