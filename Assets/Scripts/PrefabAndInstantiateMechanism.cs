aee6d4ea14a84c55a5b483c1730561f6752ae897
Burhanettin NACAR <mtbnacar@gmail.com>
2019-07-08 22:36:59 +0300

    prefab and instantiate mechanism

---
+++ Assets/Scripts/PrefabAndInstantiateMechanism.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabAndInstantiateMechanism : MonoBehaviour
+{
+    public GameObject ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // This will create a huge amount of sphere game object so i commented it out
+        // Instantiate(ball, transform.position, transform.rotation);
+    }
+}
