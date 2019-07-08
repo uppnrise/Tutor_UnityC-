75f5982ccab2d2de8bbe18c6499f146772e17896
Burhanettin NACAR <mtbnacar@gmail.com>
2019-07-08 22:01:10 +0300

    finding a single game object with tag

---
+++ Assets/Scripts/FindingGameObjectWithTag.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindingGameObjectWithTag : MonoBehaviour
+{
+    GameObject Quad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Returns a single game object based on its "Tag"
+        Quad = GameObject.FindWithTag("Quad");
+        Destroy(Quad);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
