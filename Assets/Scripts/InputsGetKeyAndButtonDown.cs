251327e5490319c8b7482cbf59157c73d6c28c22
Burhanettin NACAR <mtbnacar@gmail.com>
2019-07-10 00:56:50 +0300

    Input handling is implemented as an example

---
+++ Assets/Scripts/InputsGetKeyAndButtonDown.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputsGetKeyAndButtonDown : MonoBehaviour
+{
+    public GameObject ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if space putton pressed execute something.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(ball, transform.position, transform.rotation);
+        } // Edit->Project Settings->Input
+          // Some of the control buttons are already assigned and is usable as String parameter.
+        else if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Space pressed");
+        }
+    }
+}
