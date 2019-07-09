45be998ad1b076486512297fe5a5e1433d62ce0c
Burhanettin NACAR <mtbnacar@gmail.com>
2019-07-10 01:21:48 +0300

    Coroutines in C Sharp

---
+++ Assets/Scripts/CoroutinesInCSharp.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutinesInCSharp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float waitTime = 5f;
+        StartCoroutine(coRoutineTest(waitTime));
+        StartCoroutine("coRoutineTest", waitTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // if you a method wait and execute again
+    // you can use a coroutine.
+    IEnumerator coRoutineTest(float waitTime)
+    {
+        Debug.Log("coRoutine Started");
+
+        yield return new WaitForSeconds(5f);
+
+        Debug.Log("coRoutine Ended");
+    }
+}
