319661df00c85d84287f5f7bbebf4990fe71bc72
Burhanettin NACAR <mtbnacar@gmail.com>
2019-07-10 01:38:19 +0300

    Mouse Input and Mouse position handlers are implemented

---
+++ Assets/Scripts/MouseInputAndMousePosition.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Left mouse button pressed.");
+
+            Vector3 mousePos = Input.mousePosition;
+
+            Debug.Log(mousePos.x);
+            Debug.Log(mousePos.y);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Right mouse button pressed.");
+        }
+        if (Input.GetMouseButtonDown(2))
+        {
+            Debug.Log("Middle mouse button pressed.");
+        }
+
+
+    }
+}
