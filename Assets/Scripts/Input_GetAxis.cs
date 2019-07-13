6b925338d9af4837baa310e59be63ff9055ef93d
Burhanettin NACAR <mtbnacar@gmail.com>
2019-07-13 13:40:12 +0300

    Input Get Axis, continuous input mechanism explained

---
+++ Assets/Scripts/Input_GetAxis.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Input_GetAxis : MonoBehaviour
+{
+    float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 75f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Keep pressing the horizantal and vertical set of keys and
+        // you will move the attached object.
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        transform.Translate(x * Time.deltaTime * speed, y * Time.deltaTime * speed, 0);
+    }
+}
