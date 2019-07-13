1e70da16f3a61204faa31cf7d3461a3d96b1bb0c
Burhanettin NACAR <mtbnacar@gmail.com>
2019-07-13 14:00:20 +0300

    GetComponenet method is explained

---
+++ Assets/Scripts/GetComponentMethod.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetComponentMethod : MonoBehaviour
+{
+    /*
+     * We are able to get access to a component of a gameobject
+     * with GetComponent method. Even you can create an object
+     * from a random class and initialize it with GetComponent method.
+     */
+
+    Rigidbody rigidBody;
+    BoxCollider boxCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.velocity = new Vector3(1, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
