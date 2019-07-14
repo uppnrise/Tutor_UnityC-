e29f593b66ac216e882cc21a4303125925e1b9dc
Burhanettin NACAR <mtbnacar@gmail.com>
2019-07-14 20:33:58 +0300

    Scene Loading and Scene Management are expalined

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // Switch levels(scenes)
+            // Older versions
+            // Application.LoadLevel(0);// Put index number or scene name
+            // Default mode is single, but if we want to make it additive,
+            // we can simply use.
+            SceneManager.LoadScene("LEVEL1", LoadSceneMode.Additive);
+        }
+        
+    }
+}
